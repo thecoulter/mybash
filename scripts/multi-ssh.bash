3d357dd1d3b6143437bc8580f8e1f2a6d6054e06
webdev <webdev@glycam.org>
2022-08-03 09:40:09 -0400

    removed multi-server. Added more alias to swarm.

--- scripts/multi-ssh.bash
+++ scripts/multi-ssh.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SERVER_LIST=~/mybash/scripts/servers.txt
+SERVER_LIST=/dotfiles/mybash/scripts/servers.txt
 
 while read -u10 REMOTE_SERVER
 do
 #  ssh root@$REMOTE_SERVER 'reboot'
-   ssh -F ~/mybash/.ssh/config root@$REMOTE_SERVER $1
+   ssh webdev@$REMOTE_SERVER $1
 done 10< $SERVER_LIST
